febc5ca30f57ddf15a056627b51bffe2ae3f8e86
awitkowsky <witkowski.thebill@gmail.com>
2025-02-12 23:23:04 +0100

    ytj

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const form = document.querySelector("form");
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll("main section");
+    const navLinks = document.querySelectorAll("#nawigacja ul li a");
 
-    form.addEventListener("submit", function(event) {
-        event.preventDefault();
-        
-        const name = document.getElementById("name").value;
-        const email = document.getElementById("email").value;
-        const message = document.getElementById("message").value;
+    const observerOptions = {
+        root: null,
+        threshold: 0.60
+    };
 
-        if (name && email && message) {
-            alert("Dziękujemy za wiadomość, " + name + "!");
-            form.reset();
-        } else {
-            alert("Proszę wypełnić wszystkie pola.");
-        }
-    });
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                let activeSection = entry.target.getAttribute("id");
+
+                // Usunięcie klasy 'active' ze wszystkich linków
+                navLinks.forEach(link => {
+                    link.classList.remove("active");
+                });
+
+                // Dodanie klasy 'active' do odpowiedniego linku
+                document.querySelector(`#nawigacja ul li a[href="#${activeSection}"]`)?.classList.add("active");
+            }
+        });
+    }, observerOptions);
+
+    sections.forEach(section => observer.observe(section));
 });
